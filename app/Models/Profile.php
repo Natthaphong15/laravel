a358fbaaee4d83f85663eebe1f523031cce3f12d
Natthaphong15 <95035483+Natthaphong15@users.noreply.github.com>
2022-07-24 15:00:02 +0700

    week 10

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Profile extends Model
 {
     /**
      * The database table used by the model.
      *
      * @var string
      */
-    protected $table = 'posts';
+    protected $table = 'profiles';
 
     /**
     * The database primary key value.
@@ -25,7 +25,9 @@ class Post extends Model
      *
      * @var array
      */
-    protected $fillable = ['title', 'content', 'category', 'user_id'];
+    protected $fillable = ['no', 'type', 'issue_date', 'expire_date', 'name', 'birth_date', 'id_no', 'user_id'];
 
-    
+    public function user(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,14 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Vehicle extends Model
 {
     /**
      * The database table used by the model.
      *
      * @var string
      */
-    protected $table = 'posts';
+    protected $table = 'vehicles';
 
     /**
     * The database primary key value.
@@ -25,7 +25,9 @@ class Post extends Model
      *
      * @var array
      */
-    protected $fillable = ['title', 'content', 'category', 'user_id'];
+    protected $fillable = ['brand', 'serie', 'color', 'year', 'mileage', 'user_id'];
 
-    
+    public function user(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
